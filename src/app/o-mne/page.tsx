6f9a9a067f7d870424ef1db99600d4ebea484c06
KodiStudio36 <samko.chudy@gmail.com>
2024-09-24 11:14:17 +0200

    Initial commit

---
+++ src/app/o-mne/page.tsx
@@ -0,0 +1,17 @@
+
+// src/app/o-mne/page.tsx
+
+
+import { Typography, Container } from "@mui/material";
+
+export const metadata = {title: "O mne | Snapchat"}
+
+export default function About() {
+
+  return (
+    <Container>
+        <Typography>Stránka o mne</Typography>
+    </Container>
+  );
+
+}
---
+++ src/app/podmienky/page.tsx
@@ -0,0 +1,17 @@
+
+// src/app/podmienky/page.tsx
+
+
+import { Typography, Container } from "@mui/material";
+
+export const metadata = {title: "Podmienky | Snapchat"}
+
+export default function TermsConditions() {
+
+  return (
+    <Container>
+        <Typography>Podmienky používania</Typography>
+    </Container>
+  );
+
+}
